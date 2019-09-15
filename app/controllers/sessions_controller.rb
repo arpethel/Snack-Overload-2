9d3ea35773a99b6006a97e19857ac7887df6949a
arpethel <arpethel@gmail.com>
2019-09-15 12:13:53 -0400

    configuring login forms

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,11 @@
      
         render 'welcome/home'
       end
+
+      def destroy
+        session.delete :user_id
+        redirect_to root_path
+      end
      
       private
      
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#home'
   get '/auth/facebook/callback' => 'sessions#create_with_facebook'
+
+  resources :users, only: [:new, :create, :show]
+  get 'signup' => 'users#new'
 end
