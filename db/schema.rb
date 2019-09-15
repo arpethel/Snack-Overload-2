c389f70611957299f1ce74b9c99e3a71024aa976
arpethel <arpethel@gmail.com>
2019-09-15 15:24:21 -0400

    added admin to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_15_154601) do
+ActiveRecord::Schema.define(version: 2019_09_15_191311) do
 
   create_table "posts", force: :cascade do |t|
     t.string "caption"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_09_15_154601) do
     t.string "image"
     t.string "uid"
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
